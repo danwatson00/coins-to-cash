d0d50dc24371690ef9d6933cd27a2356bf506a0e
Dan Watson <danwatson00@yahoo.com>
2018-01-27 07:33:59 -0600

    first commit

---
+++ coinsToCash.js
@@ -0,0 +1,15 @@
+var quarters = 0;
+var dimes = 0;
+var nickels = 0;
+var pennies = 0;
+
+
+var piggyBank = {
+    quarters: 34,
+    nickels: 12,
+    dimes: 53,
+    pennies: 234
+}
+
+var dollarAmount = 0;
+dollarAmount = piggyBank.quarters * .25 + piggyBank.nickels * .05 + piggyBank.dimes * .1 + piggyBank.pennies * .01;
